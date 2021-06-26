826b13f1c7d4841d2cb4634814a82a4b2f733e34
Shay <shayc.campbell@gmail.com>
2021-06-26 18:25:17 +0100

    🏁 Downloading Dependencies
    1)NPX create-react-app

---
+++ Frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './Routes/Login';
+
+ReactDOM.render(<Login />,document.getElementById('root')
+);
+
