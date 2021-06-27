0d13f2826838ffeb0b4a9e03b81df11e7b639a65
Shay <shayc.campbell@gmail.com>
2021-06-27 04:26:15 +0100

    🗺️ Sorted Local & Global Feeds

--- Frontend/src/Components/Feed.js
+++ Frontend/src/Components/Feed.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../Styling/Feed.css'
 import { Link } from "react-router-dom";
 import AddIcon from '@material-ui/icons/Add';
+import GlobalFeed from './GlobalFeed';
+import LocalFeed from './LocalFeed';
 
 function Feed() {
     return (
@@ -11,8 +13,8 @@ function Feed() {
                 <div className="top-section">
                     <div className="holder">
                         <div className="feed-BTN">
-                            <Link><p>Your Feed</p></Link>
-                            <Link><p>Friends</p></Link>
+                            <Link to="/"><p>Your Feed</p></Link>
+                            <Link to="/Local"><p>Friends</p></Link>
                         </div>
                         <div className="create-BTN">
                             <AddIcon/>
@@ -21,7 +23,8 @@ function Feed() {
                     </div>
                 </div>
                 <Switch>
-
+                    <Route path="/" exact component={GlobalFeed}></Route>
+                    <Route path="/Local" exact component={LocalFeed}></Route>
                 </Switch>
             </Router>
         </div>
---
+++ Frontend/src/Components/LocalFeed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styling/LocalFeed.css'
+
+function LocalFeed() {
+    return (
+        <div className="local-main">
+            <div className="local-holder">
+                <h1>This feed is local</h1>
+            </div>
+        </div>
+    )
+}
+
+export default LocalFeed
