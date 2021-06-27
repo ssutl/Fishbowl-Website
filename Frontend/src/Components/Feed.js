7c74f15556bbdb926988b337e291ea00fd8076ae
Shay <shayc.campbell@gmail.com>
2021-06-27 03:44:00 +0100

    🔍 Starting Middle Feed Section

--- Frontend/src/Components/Feed.js
+++ Frontend/src/Components/Feed.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../Styling/Feed.css'
+import { Link } from "react-router-dom";
+import AddIcon from '@material-ui/icons/Add';
 
 function Feed() {
     return (
         <div className="feed-section">
-            
+            <Router>
+                <div className="top-section">
+                    <div className="holder">
+                        <div className="feed-BTN">
+                            <Link><p>Your Feed</p></Link>
+                            <Link><p>Friends</p></Link>
+                        </div>
+                        <div className="create-BTN">
+                            <AddIcon/>
+                            <p>New Room</p>
+                        </div>
+                    </div>
+                </div>
+                <Switch>
+
+                </Switch>
+            </Router>
         </div>
     )
 }
--- Frontend/src/Routes/Middle.js
+++ Frontend/src/Routes/Middle.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Feed from '../Components/Feed';
 import '../Styling/Middle.css'
-
+import SearchIcon from '@material-ui/icons/Search';
 
 function Middle() {
     return (
         <div className="middle-holder">
             <Router>
-                <div className="section1"></div>
+                <div className="section1">
+                    <div className="search-wrapper">
+                        <SearchIcon/>
+                        <input className="searchBar" type="text" placeholder="Search for rooms, users or subjects"></input>
+                    </div>
+                </div>
                 <Switch>
                     <Route path="/" exact component={Feed}/>
                 </Switch>
