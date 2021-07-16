3c1777f88364ed79d5df83e1ffaeca85020fb5ee
Shay <shayc.campbell@gmail.com>
2021-07-16 22:54:36 +0100

    ❤️ Friends Display

--- Frontend/src/Routes/SpecificUserPage.js
+++ Frontend/src/Routes/SpecificUserPage.js
@@ -1,19 +1,24 @@
 import React, {useState, useEffect} from "react";
 import { useLocation } from "react-router-dom";
 import "../Styling/specificUserPage.css"
-import axios from "axios";
+// import axios from "axios";
+import EditIcon from '@material-ui/icons/Edit';
 
 function SpecificUserPage() {
     const { state } = useLocation();
+    const [incomingFile, setIncomingFile] = useState("");
 
 
     return (
         <div className="section2">
             <div className="holder">
-                
-
-                <h1>{state.user.username}</h1>
+                    <div className="banner">
+                    </div>
+                    <div className="imageHolder">
+                        <img src={state.user.image}/>
+                    </div>
             </div>
+            
         </div>
     )
 }
