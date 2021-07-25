57d9dbce7d796fefc35472117399571d3e7cc4b0
Shay <shayc.campbell@gmail.com>
2021-07-25 01:50:47 +0100

    🔑 Create Room styling

--- Frontend/src/Components/CreateRoom.js
+++ Frontend/src/Components/CreateRoom.js
@@ -13,8 +13,7 @@ function CreateRoom() {
     const [roomExists, setRoomExists] = useState(false)
 
 
-        const handleSubmit = (event) => {
-            event.preventDefault();
+        const handleSubmit = () => {
 
             if(roomName.trim().length){
                 console.log("name is valid")
@@ -87,67 +86,36 @@ function CreateRoom() {
         <div className="section">
             <div className="section-holder">
                 <div className="title">
-                    <h1>Create Room</h1>
+                    
                 </div>
-                <form className="form">
-                    <div className="inputs">
-                        <div className="upper">
-                            <div className="roomInput">
-                                <div className="name">
-                                    <h3>Room Name</h3>
-
-                                </div>
-                                <div className="input">
-                                    <textarea type="text" placeholder="Room Name" className={roomExists?"inputField exists":"inputField"} onChange={(event)=>setRoomName(event.target.value)} required/>
-                                </div>
-                            </div>
-                            <div className="roomInput">
-                                <div className="name">
-                                    <h3>Room Question</h3>
-
-                                </div>
-                                <div className="input">
-                                    <textarea type="text" placeholder="Room Question" className="inputField" onChange={(event)=> setRoomQuestion(event.target.value)} required/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="lower">
-                            <div className="tags">
-                                <div className="titleB">
-                                    <h3>Choose Tags</h3>
-                                </div>
-                                <div className="boxes">
-                                    <div className="box-holder">
-                                        <div className={Math?"tag selected":"tag"} onClick={()=>setMath(!Math)}>#Math</div>
-                                        <div className={English?"tag selected":"tag"} onClick={()=>setEnglish(!English)}>#English</div>
-                                        <div className={Geography?"tag selected":"tag"} onClick={()=>setGeography(!Geography)}>#Geography</div>
-                                        <div className={FM?"tag selected":"tag"} onClick={()=>setFM(!FM)}>#FM</div>
-                                        <div className={CS?"tag selected":"tag"} onClick={()=>setCS(!CS)}>#CS</div>
-                                        <div className={Economics?"tag selected":"tag"} onClick={()=>setEconomics(!Economics)}>#Economics</div>
-                                        <div className={History?"tag selected":"tag"} onClick={()=>setHistory(!History)}>#History</div>
-                                        <div className={Biology?"tag selected":"tag"} onClick={()=>setBiology(!Biology)}>#Biology</div>
-                                        <div className={Psychology?"tag selected":"tag"} onClick={()=>setPsychology(!Psychology)}>#Psychology</div>
-                                        <div className={Physics?"tag selected":"tag"} onClick={()=>setPhysics(!Physics)}>#Physics</div>
-                                        <div className={Polotics?"tag selected":"tag"} onClick={()=>setPolotics(!Polotics)}>#Polotics</div>
-                                        <div className={Music?"tag selected":"tag"} onClick={()=>setMusic(!Music)}>#Music</div>
-                                        <div className={RE?"tag selected":"tag"} onClick={()=>setRE(!RE)}>#RE</div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
+                <div className="inputs">
+                    <input type="input" className={roomExists?"input_field exists":"input_field"} onChange={(event)=>setRoomName(event.target.value)} required id="name" placeholder="Room Name" />
+                    <input type="input"  className="input_field" id="question" placeholder="Room Question" onChange={(event)=> setRoomQuestion(event.target.value)} required/>
+                </div>
+                <div className="boxes">
+                    <div className="box-holder">
+                        <div className={Math?"tag selected":"tag"} onClick={()=>setMath(!Math)}>#Math</div>
+                        <div className={English?"tag selected":"tag"} onClick={()=>setEnglish(!English)}>#English</div>
+                        <div className={Geography?"tag selected":"tag"} onClick={()=>setGeography(!Geography)}>#Geography</div>
+                        <div className={FM?"tag selected":"tag"} onClick={()=>setFM(!FM)}>#FM</div>
+                        <div className={CS?"tag selected":"tag"} onClick={()=>setCS(!CS)}>#CS</div>
+                        <div className={Economics?"tag selected":"tag"} onClick={()=>setEconomics(!Economics)}>#Economics</div>
+                        <div className={History?"tag selected":"tag"} onClick={()=>setHistory(!History)}>#History</div>
+                        <div className={Biology?"tag selected":"tag"} onClick={()=>setBiology(!Biology)}>#Biology</div>
+                        <div className={Psychology?"tag selected":"tag"} onClick={()=>setPsychology(!Psychology)}>#Psychology</div>
+                        <div className={Physics?"tag selected":"tag"} onClick={()=>setPhysics(!Physics)}>#Physics</div>
+                        <div className={Polotics?"tag selected":"tag"} onClick={()=>setPolotics(!Polotics)}>#Polotics</div>
+                        <div className={Music?"tag selected":"tag"} onClick={()=>setMusic(!Music)}>#Music</div>
+                        <div className={RE?"tag selected":"tag"} onClick={()=>setRE(!RE)}>#RE</div>
                     </div>
-                    <div className="upload">
-                        <input type="submit" id="uploadBtn">
-
-                        </input>
-                            <label htmlFor="uploadBtn" className="uploadLabel" onClick={(event)=>handleSubmit(event)}>
-                                <h2>Create</h2>
-                            </label>
+                </div>
+                <div className="submit">
+                    <div className="BTN" onClick={()=>handleSubmit()}>
+                        <h3>Create</h3>
                     </div>
-                </form>
+                </div>
+                 
             </div>
-            
         </div>
     )
 }
